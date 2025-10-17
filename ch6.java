64f38c6a7a9cfaf3b0fd67421d896b15c6d2506b
LemonPrism <charliechenvan@gmail.com>
2025-10-16 21:40:59 -0700

    Add files via upload

--- ch6.java
+++ ch6.java
@@ -1,19 +1,94 @@
+/*
 
+public class ch6 {
+    public static void main(String[] args) {
+        String msg = "I have a pen, I have a Apple.";
+        System.out.println(ppap(msg));  
+    }
 
-public class ch6
-{
-   public static void main ( String []args){
-      String msg = "I have a pen, I have a Apple"; 
-      System.out.println(PPAP(msg)); 
-   }
-   public static String PPAP (String msg ){
+    public static String ppap(String msg) {
+        String G = "I have a ";
+        String C = ","; 
+        String P= "."; 
+               
        
-       String obj1= msg.indexOf("I have a" ); 
-       String obj2 = msg.indexOf("I have a" ); 
-       
-       
-       return "Uh!"+ obj2 + obj1; 
-       
-       
-   }
+        
+        int c = msg.indexOf(C); 
+        String obj1 = msg.substring (8, c ); 
+        
+      
+        int Sc = msg.indexOf(P, c); 
+        String obj2 = msg.substring (c+11, Sc );
+        
+        
+        return "Uh! "+obj2 + obj1 ;
+    }
 }
+*/ 
+/*
+public class ch6 {
+    public static void main(String[] args) {
+        
+        String msg = "Amy, must I jujitsu my ma?";
+        if (isPalindrome(msg)){
+            System.out.println(msg + " is a palindrome"); 
+        } else {
+            System.out.println(msg + " is not a palindrome");
+        }
+    }    
+
+    public static boolean isPalindrome(String str) {
+        String Lstr = str.toLowerCase(); 
+        String m = ""; 
+        String nonLetters = ",.:;()[]{}!@#$%^&* ";
+        
+        if (nonLetters.contains(str)) {
+            
+            
+            
+            
+        
+        }
+
+
+
+        for (int i = Lstr.length() - 1; i >= 0; i--) {
+            m += Lstr.charAt(i);
+        }
+        if(m.equals(Lstr)){
+            return true; 
+        }else {
+            return false; 
+        
+        }
+    }
+}
+
+*/
+
+public class ch6 {
+    public static void main(String[] args) {
+        
+        int num = 124; 
+        
+        System.out.println( convertToBinary(num)); 
+        
+       
+    }    
+
+    public static String convertToBinary (int num) {
+        String m = "" ; 
+        
+        if ( num==0){
+            return "0"; 
+        }
+        
+        for (int i = num; i > 0; i /= 2){
+            m = (i%2)+m;
+            
+        }
+        
+        return m ; 
+    }
+}
+
